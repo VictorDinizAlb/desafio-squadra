62dbb9b260f8605e1965be018d5af4911774de64
Victor Mateus <victordiniz_@outlook.com>
2022-01-26 15:34:02 -0400

    test connection

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,3 +1,44 @@
 import { createConnection } from "typeorm";
 
 createConnection();
+console.log('chegou');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const oracledb = require('oracledb');
+
+// oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+// const mypw = 'senha'  // set mypw to the hr schema password
+
+// async function run() {
+
+//   let connection;
+
+//   try {
+//     connection = await oracledb.getConnection( {
+//       user          : "C##VICTOR",
+//       password      : mypw,
+//       connectString : "localhost/xe"
+//     });
+
+//     const result = await connection.execute(
+//       `SELECT manager_id, department_id, department_name
+//        FROM departments
+//        WHERE manager_id = :id`,
+//       [103],  // bind value for :id
+//     );
+//     console.log(result.rows);
+
+//   } catch (err) {
+//     console.error(err);
+//   } finally {
+//     if (connection) {
+//       try {
+//         await connection.close();
+//       } catch (err) {
+//         console.error(err);
+//       }
+//     }
+//   }
+// }
+
+// run();
